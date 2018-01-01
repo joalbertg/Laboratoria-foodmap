47e2df1f8fc4a3ad7a9e9b4f8fc8878880b2d948
Joalbert González <joalbertgonzalez@gmail.com>
2018-01-01 12:22:10 -0500

    added: scripts, html directory and assets

--- js/app.js
+++ js/app.js
@@ -1,5 +1,72 @@
 function begin() {
-  alert('jQuery ready');
+  // contenedor principal
+  var $container = $('.container-search');
+
+  $('input').prop('disabled', true);
+
+  // funcion para mostrar los restaurantes
+  function showPoints() {
+    var str = '';
+    var template = '<figure>' +
+      '<img class="img-fluid" src="__img__" alt="__alt__" data-pos="__pos__">' +
+      '</figure>';
+
+    $container.html('');
+
+    for (var i = 0; i < data.length; i++) {
+      str += template
+        .replace('__img__', data[i].img)
+        .replace('__alt__', data[i].eslogan)
+        .replace('__pos__', i);
+    }
+    $container.html(str);
+  }
+  // llamada de la funcion para mostrar las imagenes
+  showPoints();
 }
 
 $(document).ready(begin);
+
+$(window).on('load', function() {
+  $('input').prop('disabled', false);
+
+  // algún efecto con mouseover
+  $('.container-search img').on('mouseover', function() {
+    $(this).fadeOut().fadeIn();
+  });
+
+  // despliegue del modal
+  $('.container-search img').on('click', modalDetail);
+
+  // filtro de restaurantes
+  $('#search').on('keyup', search);
+
+  function modalDetail() {
+    var $modal = $('#exampleModalCenter');
+    var element = data[parseInt($(this).data('pos'))];
+
+    $modal.find('#exampleModalLongTitle').text(element.nombre);
+    $modal.find('h3').text($(this).attr('alt'));
+
+    var $address = $modal.find('.detail-js').children().eq(0).children().first();
+    var $other = $modal.find('.detail-js').children().eq(0).children().last();
+
+    $address.text(element.direccion);
+    $other.text(element.precios);
+
+    $modal.modal('show');
+  }
+
+  // filtramos la selección
+  function search() {
+    if ($(this).val()) {
+      $('.container-search img').each(function(index) {
+        if (!data[index].tags.match($('input').val())) {
+          $(this).hide();
+        }
+      });
+    } else {
+      $('.container-search img').show();
+    }
+  }
+});
---
+++ js/data.js
@@ -0,0 +1,34 @@
+var data = [
+  {
+    nombre: 'primer restaurante',
+    eslogan: 'eslogan 1',
+    direccion: 'alguna dirección',
+    precios: '10 - 100 /s',
+    tags: 'peruana familiar bebidas personal ejecutiva ceviche tacos',
+    img: 'http://lorempixel.com/400/200/'
+  },
+  {
+    nombre: 'segundo restaurante',
+    eslogan: 'eslogan 2',
+    direccion: 'alguna dirección',
+    precios: '20 - 200 /s',
+    tags: 'bebidas personal ejecutiva tacos pastas',
+    img: 'http://lorempixel.com/400/200/'
+  },
+  {
+    nombre: 'tercero restaurante',
+    eslogan: 'eslogan 3',
+    direccion: 'alguna dirección',
+    precios: '30 - 300 /s',
+    tags: 'peruana familiar ceviche carnes',
+    img: 'http://lorempixel.com/400/200/'
+  },
+  {
+    nombre: 'cuarto restaurante',
+    eslogan: 'eslogan 4',
+    direccion: 'alguna dirección',
+    precios: '40 - 400 /s',
+    tags: 'vegetarianas',
+    img: 'http://lorempixel.com/400/200/'
+  }
+];
